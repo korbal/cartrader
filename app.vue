581dea4cf24ed2a2402963f3e03e73a9ce0f524b
korbal <balint.j.korosi@gmail.com>
2022-12-16 15:56:49 +0100

    5th done

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <NuxtWelcome />
+  <!-- NAVBAR -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 borderb-b bg-white p-4 shadow-md">
+      <a href="" class="text-3xl font-mono">cartrader</a>
+    </header>
+  <!-- NAVBAR END-->
+    
   </div>
 </template>
