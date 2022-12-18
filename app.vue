48e18ddfce8712195cb3d603b581a14aa4c28cb5
korbal <balint.j.korosi@gmail.com>
2022-12-18 14:50:23 +0100

    7th done

--- app.vue
+++ app.vue
@@ -1,10 +1,4 @@
 <template>
   <div>
-  <!-- NAVBAR -->
-    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 borderb-b bg-white p-4 shadow-md">
-      <a href="" class="text-3xl font-mono">cartrader</a>
-    </header>
-  <!-- NAVBAR END-->
-    
   </div>
 </template>
