0696c50fcf97f6e6b62c5b89a155785ed1b10af6
korbal <balint.j.korosi@gmail.com>
2022-12-19 14:36:09 +0100

    12th done

---
+++ components/CarSearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+    <div
+          class="font-serif w-[1000px] text 2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+          <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+            placeholder="Search by city...">
+          <button class="bg-[#f9a109] px w-32 h-16 text-white text-2xl">Search</button>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <!-- NAVBAR -->
+  <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 borderb-b bg-white p-4 shadow-md">
+    <a href="" class="text-3xl font-mono">cartrader</a>
+  </header>
+  <!-- NAVBAR END-->
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
